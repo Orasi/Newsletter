0e6a63c0a14d4046e3ac463e86c31ff07f1879bc
ryderstorm <damien.r.storm@gmail.com>
2014-10-30 16:02:46 +0000

    updating to use new repository location

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'rr_q1_fy15'
-set :repo_url, 'git@github.com:ryderstorm/rural_reader_q1.git'
+set :repo_url, 'git@github.com:Orasi/Newsletter.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
