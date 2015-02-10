6166a9c9e805718128ccd2f877efa8c4c86912ec
Damien Storm <damien.r.storm@gmail.com>
2015-02-10 17:17:11 +0000

    updated comments, added variables for symlink locations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,37 @@
+# This file contains the settings for creating the production version of the website
+# that is hosted at [http://ruraldirectory.orasi.com/rural_reader/].
+# To deploy it, run the command "bundle exec cap production deploy".
+# It will build the production site from your master git branch, regardless of
+# your current working branch
+#
+# IMPORTANT: make sure that [folder_to_deploy] is correctly set to the
+# date of the CURRENT edition that is already out or coming out for the first time
+# NOT a future or past edition
+#
+# Also make sure that all changes have been pushed to master on git, otherwise
+# they won't be reflected on the live production site.
+
 # Set the branch to master
 branch_to_deploy = 'master'
 set :branch, branch_to_deploy
 
 # Set the deploy folder to be the location of the current version
 # this will need to be updated whenever a new issue needs to be deployed
-# !!!!make sure you update this BEFORE running cap production deploy!!!!
-# The symbolic link "current" in /var/www/rural-reader will also need to be updated
-# to match the 'current' folder in this location
+# !!!!Make sure you update this BEFORE running cap production deploy!!!!
 folder_to_deploy = '/var/www/rural-reader/2015/q2'
 set :deploy_to, folder_to_deploy
 
-# Display a status message with the selected options
+# Set the name and location of the symlink
+# The webserver uses symbolic links to know where the content is for the link:
+# http://ruraldirectory.orasi.com/rural_reader/
+# Capistrano uses these settings to delete the old symlink and create a new
+# one with the updated content
+symlink_name = '/var/www/rural-reader/current'
+set :sym_name, symlink_name
+symlink_location = "#{folder_to_deploy}/current/"
+set :sym_location, symlink_location
+
+# Display a status message with the selected options and validate with the user
 puts "\nDeploying branch: #{branch_to_deploy} to folder: #{folder_to_deploy}\n"
 puts "\n\e[0;33mIs this the correct confiuration?\e[0m"
 set :response, ask("\e[0;32m'Y'\e[0m to continue,\e[0;31m 'N'\e[0m to abort.", nil)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,14 +1,34 @@
-# Set the deploy branch to the current branch
+# This file contains the settings for creating the staging version of the website
+# that is hosted at [http://ruraldirectory.orasi.com/next_rural_reader/].
+# To deploy it, run the command "bundle exec cap staging deploy".
+# It will build the staging site from your current working git branch, which
+# should NOT be the master branch - this is the staging environment and
+# should be a work in progress
+#
+# IMPORTANT: make sure that [folder_to_deploy] is correctly set to the
+# date of the NEXT edition that is about to come out, NOT the current one
+#
+# Also make sure that you have committed any changes to the site to the repository
+# or they won't be reflected on the live staging site.
 
+# Set the deploy branch to the current branch
 branch_to_deploy = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
 set :branch, branch_to_deploy
 
-# Set the deploy folder to be the location of the next version 
-# this will need to be updated whenever a new issue needs to be deployed
-# The symbolic link "next" in /var/www/rural-reader will also need to be updated
-# to match 'current' folder in this location
+# Set the deploy folder to be the location of the next version while in development.
+# This will need to be updated whenever a new issue needs to be deployed
 folder_to_deploy = '/var/www/rural-reader/2015/q3'
 set :deploy_to, folder_to_deploy
+
+# Set the name and location of the symlink
+# The webserver uses symbolic links to know where the content is for the link:
+# http://ruraldirectory.orasi.com/next_rural_reader/
+# Capistrano uses these settings to delete the old symlink and create a new
+# one with the updated content
+symlink_name = '/var/www/rural-reader/next'
+set :sym_name, symlink_name
+symlink_location = "#{folder_to_deploy}/current/"
+set :sym_location, symlink_location
 
 # Display a status message with the selected options and validate with the user
 puts "\nDeploying branch: #{branch_to_deploy} to folder: #{folder_to_deploy}\n"
