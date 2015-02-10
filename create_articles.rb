9de87d07cf4ac991eacbed3d71903fb72046505f
Damien Storm <damien.r.storm@gmail.com>
2015-02-10 20:46:07 +0000

    small logic change to remove colons from article titles

--- create_articles.rb
+++ create_articles.rb
@@ -14,7 +14,7 @@ File.readlines('article_list').each do |l|
   filename = title.downcase.gsub(/[^0-9a-z]+/, '_')
   full_filename = "_#{filename}_article.html.haml"
   articles << full_filename
-  article_data = "#{filename}:\n  title: #{title}\n  author: #{author}"
+  article_data = "#{filename}:\n  title: #{title.gsub(':', ' -')}\n  author: #{author}"
   open(articles_file, 'a') { |f| f.puts article_data }
   open("articles/content/#{full_filename}", 'a') do |f|
     puts "Created/appended article file: #{full_filename}"
@@ -28,6 +28,6 @@ article_files = Dir.glob('articles/content/*.haml')
 article_files.each do |f|
   unless articles.include?(File.basename(f))
     puts "File [#{File.basename(f)}] isn't in the article list, so its being archived."
-    File.rename(f, "articles/archived/#{File.basename(f)}") 
+    File.rename(f, "articles/archived/#{File.basename(f)}")
   end
 end
